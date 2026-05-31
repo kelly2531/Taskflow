0426a60cae036a452db97e2be268932663cd42c2
kelly2531 <kellycardoso2531@gmail.com>
2026-05-31 17:15:20 -0300

    Adiciona helmet e remove x-powered-by para segurança

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const helmet = require("helmet");
 
 const db = require('./config/db');
 const authRoutes = require('./routes/auth');
@@ -8,12 +9,22 @@ const taskRoutes = require('./routes/tasks');
 
 const app = express();
 
-app.use(cors());
+// segurança primeiro
+app.use(helmet());
+app.disable("x-powered-by");
+
+// middlewares
+app.use(cors({
+  origin: "https://taskflow-kadn.onrender.com"
+}));
+
 app.use(express.json());
 
+// arquivos estáticos
 app.use(express.static('public'));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// rotas
 app.use('/api', authRoutes);
 app.use('/api', taskRoutes);
 
